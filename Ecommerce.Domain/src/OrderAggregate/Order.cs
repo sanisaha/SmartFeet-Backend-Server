a4c91a06becfb415f27499c292a5d0c5cfa896d5
qtatsa <aththanayake.sandaminiaththanayake@tuni.fi>
2024-08-26 17:34:28 +0300

    Order aggregate included

---
+++ Ecommerce.Domain/src/OrderAggregate/Order.cs
@@ -0,0 +1,59 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Domain.src.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal TotalPrice { get; set; }
+
+        [ForeignKey("Address")]
+        public Guid ShippingAddressId { get; set; }
+
+        [Required]
+        public OrderStatus OrderStatus { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual Address Address { get; set; }
+
+
+        // Constructor
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+
+        public void AddOrderItem(OrderItem item)
+        {
+            OrderItems.Add(item);
+            TotalPrice += item.Price * item.Quantity;
+        }
+
+        public void RemoveOrderItem(OrderItem item)
+        {
+            if (OrderItems.Remove(item))
+            {
+                TotalPrice -= item.Price * item.Quantity;
+            }
+        }
+
+        public void UpdateOrderStatus(OrderStatus status)
+        {
+            OrderStatus = status;
+        }
+    }
+}
