6532fff7fbabdc47dab4b7fc42148a5e3874161c
Mohamad Nachawati <75593627+MohamadNach@users.noreply.github.com>
2024-08-26 10:01:22 +0300

    chore

--- Ecommerce.Service/obj/Debug/net8.0/Ecommerce.Service.AssemblyInfo.cs
+++ Ecommerce.Service/obj/Debug/net8.0/Ecommerce.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432dbb463559a159b0c244082e9d0451180fc6bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203207301413fec15c2f5327c9ef134d803ff381")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
