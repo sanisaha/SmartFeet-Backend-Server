441ec25ab5fa57050b78bf65c3f46a0110e9fe00
qtatsa <aththanayake.sandaminiaththanayake@tuni.fi>
2024-08-28 19:46:14 +0300

    category service implemented

--- Ecommerce.Service/src/CategoryService/ICategoryManagement.cs
+++ Ecommerce.Service/src/CategoryService/ICategoryManagement.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ecommerce.Domain.src.CategoryAggregate;
 using Ecommerce.Service.src.Shared;
 
@@ -9,10 +5,10 @@ namespace Ecommerce.Service.src.CategoryService
 {
     public interface ICategoryManagement : IBaseService<Category, CategoryReadDto, CategoryCreateDto, CategoryUpdateDto>
     {
-        Task<CategoryCreateDto> CreateAsync(CategoryCreateDto createDto);
-        Task<CategoryUpdateDto> UpdateAsync(Guid id, CategoryUpdateDto updateDto);
+        Task<Category> CreateAsync(CategoryCreateDto createDto);
+        Task<bool> UpdateAsync(Guid id, CategoryUpdateDto updateDto);
         Task<CategoryReadDto> GetByIdAsync(Guid id);
-        public Task DeleteAsync(Guid id);
+        Task<bool> DeleteAsync(Guid id);
 
     }
 }
--- Ecommerce.Service/src/OrderService/IOrderService.cs
+++ Ecommerce.Service/src/OrderService/IOrderService.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ecommerce.Service.src.Shared;
 
 namespace Ecommerce.Service.src.OrderService
