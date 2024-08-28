d6eebc5c8ec5990115ee0ce43cb80f2d2b6f730a
qtatsa <aththanayake.sandaminiaththanayake@tuni.fi>
2024-08-29 00:13:49 +0300

    Implemented service related order items

--- Ecommerce.Service/src/OrderItemService/IOrderItemManagement.cs
+++ Ecommerce.Service/src/OrderItemService/IOrderItemManagement.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ecommerce.Service.src.OrderService;
 
 namespace Ecommerce.Service.src.OrderItemService
 {
     public interface IOrderItemManagement
     {
-        
+        Task<OrderCreateDto> CreateAsync(OrderCreateDto createDto);
+        Task<bool> UpdateAsync(Guid id, OrderUpdateDto updateDto);
+        Task<OrderReadDto> GetByIdAsync(Guid id);
+        public Task DeleteAsync(Guid id);
+        Task<IEnumerable<OrderReadDto>> GetOrderItemsByOrderIdAsync(Guid orderId);
+        Task<IEnumerable<OrderReadDto>> GetOrderItemsByProductIdAsync(Guid productId);
+        Task<int> GetTotalQuantityByOrderIdAsync(Guid orderId);
     }
 }
--- Ecommerce.Service/src/OrderService/OrderManagement.cs
+++ Ecommerce.Service/src/OrderService/OrderManagement.cs
@@ -201,6 +201,5 @@ namespace Ecommerce.Service.src.OrderService
 
             }
         }
-
     }
 }
