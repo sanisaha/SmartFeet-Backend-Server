52760ec40d4259f46fd12f2e88c5e49fd0e5dd42
sanisaha <ratulcvasu@gmail.com>
2024-08-29 15:23:49 +0300

    implement BaseService and BaseRepository for Review

--- Ecommerce.Service/src/ReviewService/ReviewManagement.cs
+++ Ecommerce.Service/src/ReviewService/ReviewManagement.cs
@@ -13,75 +13,6 @@ namespace Ecommerce.Service.src.ReviewService
         {
             _reviewRepository = reviewRepository;
         }
-        public async Task<Review> CreateAsync(ReviewCreateDto createDto)
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(createDto.ReviewText))
-                    throw new ArgumentException("Review text is required.");
-
-                var reviewEntity = createDto.CreateEntity();
-                return await _reviewRepository.CreateAsync(reviewEntity);
-
-            }
-            catch (Exception)
-            {
-                throw new Exception("Error creating review!.");
-            }
-        }
-
-        public async Task DeleteAsync(Guid id)
-        {
-            try
-            {
-                var review = await _reviewRepository.GetAsync(r => r.Id == id);
-                if (review == null)
-                    throw new ArgumentException("Review not found.");
-
-                await _reviewRepository.DeleteByIdAsync(id);
-            }
-            catch (Exception)
-            {
-                throw new Exception("Error deleting review!.");
-            }
-        }
-
-        public async Task<IEnumerable<ReviewReadDto>> GetAllReviewsAsync()
-        {
-            try
-            {
-                var reviews = await _reviewRepository.GetAllReviewsAsync();
-                return reviews.Select(r =>
-                {
-                    var reviewDto = new ReviewReadDto();
-                    reviewDto.FromEntity(r);
-                    return reviewDto;
-                });
-            }
-            catch (Exception)
-            {
-                throw new Exception("Error getting reviews!.");
-            }
-        }
-
-        public async Task<ReviewReadDto> GetReviewByIdAsync(Guid reviewId)
-        {
-            try
-            {
-                var review = await _reviewRepository.GetAsync(r => r.Id == reviewId);
-                if (review == null)
-                    throw new ArgumentException("Review not found.");
-
-                var reviewDto = new ReviewReadDto();
-                reviewDto.FromEntity(review);
-
-                return reviewDto;
-            }
-            catch (Exception)
-            {
-                throw new Exception("Error getting review!.");
-            }
-        }
 
         public async Task<IEnumerable<ReviewReadDto>> GetReviewsByProductIdAsync(Guid productId)
         {
@@ -116,29 +47,6 @@ namespace Ecommerce.Service.src.ReviewService
             catch (Exception)
             {
                 throw new Exception("Error getting reviews!.");
-            }
-        }
-
-        public async Task<ReviewUpdateDto> UpdateAsync(Guid id, ReviewUpdateDto updateDto)
-        {
-            try
-            {
-                var existingReview = await _reviewRepository.GetAsync(r => r.Id == id);
-                if (existingReview == null)
-                    throw new ArgumentException("Review not found.");
-
-                if (string.IsNullOrWhiteSpace(updateDto.ReviewText))
-                    throw new ArgumentException("Review text is required.");
-
-                var updatedReview = updateDto.UpdateEntity(existingReview);
-
-                await _reviewRepository.UpdateByIdAsync(updatedReview);
-
-                return updateDto;
-            }
-            catch (Exception)
-            {
-                throw new Exception("Error updating review!.");
             }
         }
     }
