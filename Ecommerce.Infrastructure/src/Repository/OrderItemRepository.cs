616c28ae7d46b06ec588ad9052ff7ecf002cd895
sanisaha <ratulcvasu@gmail.com>
2024-08-29 19:29:21 +0300

    implement basic services for order Item

--- Ecommerce.Infrastructure/src/Repository/OrderItemRepository.cs
+++ Ecommerce.Infrastructure/src/Repository/OrderItemRepository.cs
@@ -7,57 +7,13 @@ using Microsoft.EntityFrameworkCore;
 namespace Ecommerce.Infrastructure.src.Repository
 
 {
-    public class OrderItemRepository : IOrderItemRepository
+    public class OrderItemRepository : BaseRepository<OrderItem>, IOrderItemRepository
     {
         private readonly ApplicationDbContext _context;
 
-        public OrderItemRepository(ApplicationDbContext context)
+        public OrderItemRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
-        }
-
-        public async Task<OrderItem> CreateAsync(OrderItem entity)
-        {
-            _context.OrderItems.Add(entity);
-            await _context.SaveChangesAsync();
-            return entity;
-        }
-
-        public async Task<bool> UpdateByIdAsync(OrderItem entity)
-        {
-            _context.OrderItems.Update(entity);
-            var result = await _context.SaveChangesAsync();
-            return result > 0;
-        }
-
-        public async Task<bool> DeleteByIdAsync(Guid id)
-        {
-            var orderItem = await _context.OrderItems.FindAsync(id);
-            if (orderItem == null)
-            {
-                return false;
-            }
-
-            _context.OrderItems.Remove(orderItem);
-            var result = await _context.SaveChangesAsync();
-            return result > 0;
-        }
-
-        public async Task<OrderItem> GetAsync(Expression<Func<OrderItem, bool>>? filter = null, bool tracked = true)
-        {
-            IQueryable<OrderItem> query = _context.OrderItems;
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-
-            return await query.FirstOrDefaultAsync();
         }
 
         public async Task<IEnumerable<OrderItem>> GetOrderItemsByOrderIdAsync(Guid orderId)
@@ -79,11 +35,6 @@ namespace Ecommerce.Infrastructure.src.Repository
             return await _context.OrderItems
                 .Where(oi => oi.OrderId == orderId)
                 .SumAsync(oi => oi.Quantity);
-        }
-
-        public async Task<IEnumerable<OrderItem>> GetAllAsync()
-        {
-            return await _context.OrderItems.ToListAsync();
         }
     }
 }
