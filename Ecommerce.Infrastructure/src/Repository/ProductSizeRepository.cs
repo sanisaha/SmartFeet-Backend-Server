461a1bc125ce232d0149f865d25822705cb35db3
sanisaha <ratulcvasu@gmail.com>
2024-08-29 18:53:24 +0300

    implement basic services for product size

--- Ecommerce.Infrastructure/src/Repository/ProductSizeRepository.cs
+++ Ecommerce.Infrastructure/src/Repository/ProductSizeRepository.cs
@@ -4,83 +4,27 @@ using Ecommerce.Domain.src.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Ecommerce.Infrastructure.src.Database;
 using Ecommerce.Domain.Enums;
+using Ecommerce.Infrastructure.src.Repository;
 
 namespace Ecommerce.Infrastructure.Repositories
 {
-    public class ProductSizeRepository : IProductSizeRepository
+    public class ProductSizeRepository : BaseRepository<ProductSize>, IProductSizeRepository
     {
         private readonly ApplicationDbContext _context;
 
-        public ProductSizeRepository(ApplicationDbContext context)
+        public ProductSizeRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
         }
 
-        public async Task<ProductSize> CreateAsync(ProductSize entity)
-        {
-            _context.ProductSizes.Add(entity);
-            await _context.SaveChangesAsync();
-            return entity;
-        }
-
-        public async Task<bool> UpdateByIdAsync(ProductSize entity)
-        {
-            _context.ProductSizes.Update(entity);
-            var result = await _context.SaveChangesAsync();
-            return result > 0;
-        }
-
-        public async Task<bool> DeleteByIdAsync(Guid id)
-        {
-            var productSize = await _context.ProductSizes.FindAsync(id);
-            if (productSize == null)
-            {
-                return false;
-            }
-
-            _context.ProductSizes.Remove(productSize);
-            var result = await _context.SaveChangesAsync();
-            return result > 0;
-        }
-
-        public async Task<ProductSize> GetAsync(Expression<Func<ProductSize, bool>>? filter = null, bool tracked = true)
-        {
-            IQueryable<ProductSize> query = _context.ProductSizes;
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-
-            return await query.FirstOrDefaultAsync();
-        }
-
         public async Task<IEnumerable<ProductSize>> GetSizesByProductIdAsync(Guid productId)
         {
-            return await _context.ProductSizes
-                .Where(ps => ps.ProductId == productId)
-                .ToListAsync();
+            return await _context.ProductSizes.Where(ps => ps.Product.Id == productId).ToListAsync();
         }
 
-        public async Task<ProductSize> GetSizeByNameAsync(SizeValue sizeValue)
+        public async Task<ProductSize> GetSizeByValueAsync(SizeValue size)
         {
-            return await _context.ProductSizes
-                .FirstOrDefaultAsync(ps => ps.SizeValue == sizeValue);
-        }
-
-        public Task<ProductSize> GetSizeByValueAsync(SizeValue sizeValue)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<ProductSize>> GetAllAsync()
-        {
-            return await _context.ProductSizes.ToListAsync();
+            return await _context.ProductSizes.FirstOrDefaultAsync(ps => ps.SizeValue == size);
         }
     }
 }
--- Ecommerce.Service/src/ProductSizeService/ProductSizeManagement.cs
+++ Ecommerce.Service/src/ProductSizeService/ProductSizeManagement.cs
@@ -15,46 +15,7 @@ namespace Ecommerce.Service.src.ProductSizeService
             _productSizeRepository = productSizeRepository;
             _productRepository = productRepository;
         }
-        public async Task<ProductSize> CreateAsync(ProductSizeCreateDto createDto)
-        {
-            try
-            {
-                if (!Enum.IsDefined(typeof(SizeValue), createDto.SizeValue))
-                    throw new ArgumentException("Invalid size value.");
 
-
-                var product = await _productRepository.GetAsync(p => p.Id == createDto.ProductId);
-                if (product == null)
-                    throw new ArgumentException("Invalid product.");
-
-                var productSizeEntity = createDto.CreateEntity();
-
-                return await _productSizeRepository.CreateAsync(productSizeEntity);
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error creating Product Size!.");
-            }
-        }
-
-        public async Task<bool> DeleteAsync(Guid id)
-        {
-            try
-            {
-                var productSize = await _productSizeRepository.GetAsync(ps => ps.Id == id);
-                if (productSize == null)
-                    throw new ArgumentException("Product size not found.");
-
-                return await _productSizeRepository.DeleteByIdAsync(id);
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error deleting Product Size");
-            }
-
-        }
 
         public async Task<IEnumerable<ProductSizeReadDto>> GetSizesByProductIdAsync(Guid productId)
         {
@@ -81,27 +42,16 @@ namespace Ecommerce.Service.src.ProductSizeService
             }
         }
 
-        public async Task<bool> UpdateAsync(Guid id, ProductSizeUpdateDto updateDto)
+        public async Task<ProductSize> GetSizeByValueAsync(SizeValue sizeValue)
         {
             try
             {
-                var existingSize = await _productSizeRepository.GetAsync(ps => ps.Id == id);
-                if (existingSize == null)
-                    throw new ArgumentException("Product size not found.");
-
-                if (!Enum.IsDefined(typeof(SizeValue), updateDto.SizeValue))
-                    throw new ArgumentException("Invalid size value.");
-
-                var product = await _productRepository.GetAsync(p => p.Id == updateDto.ProductId);
-                if (product == null)
-                    throw new ArgumentException("Invalid product.");
-
-                var productSizeToUpdate = updateDto.UpdateEntity(existingSize);
-                return await _productSizeRepository.UpdateByIdAsync(productSizeToUpdate);
+                var size = await _productSizeRepository.GetSizeByValueAsync(sizeValue);
+                return size;
             }
             catch (Exception ex)
             {
-                throw new Exception($"Error updating Product Size: {ex.Message}", ex);
+                throw new Exception($"Error retrieving Product Size!.");
             }
         }
     }
